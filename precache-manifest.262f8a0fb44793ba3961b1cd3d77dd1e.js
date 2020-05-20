9268810a8cd99a534735d82b6ea7f2e7e600cd7a
SanteraSan <santera102@gmail.com>
2020-05-20 18:50:39 +0500

    Updates

---
+++ precache-manifest.262f8a0fb44793ba3961b1cd3d77dd1e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc883de593f04ce952c31b38823389ff",
+    "url": "/ReactJS-learn/index.html"
+  },
+  {
+    "revision": "c1c23096c31813c384d9",
+    "url": "/ReactJS-learn/static/css/main.8b9e708a.chunk.css"
+  },
+  {
+    "revision": "e1406f440351cdf2d0eb",
+    "url": "/ReactJS-learn/static/js/2.aacba89b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/ReactJS-learn/static/js/2.aacba89b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c1c23096c31813c384d9",
+    "url": "/ReactJS-learn/static/js/main.e78c9521.chunk.js"
+  },
+  {
+    "revision": "cb737f1b8ec413e01209",
+    "url": "/ReactJS-learn/static/js/runtime-main.92958a67.js"
+  },
+  {
+    "revision": "9064981b80e59f1b663c17fab41563c9",
+    "url": "/ReactJS-learn/static/media/Spinner-1s-200px.9064981b.svg"
+  },
+  {
+    "revision": "7fc247f259b364750eb1d098d4fc5226",
+    "url": "/ReactJS-learn/static/media/png-transparent-computer-icons-user-icon-design-help-portal-computer-orange-sphere.7fc247f2.png"
+  }
+]);
